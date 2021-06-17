58a0b834a05b25096b6758b546ef43b7e909186e
kryderius <krydegamer@gmail.com>
2021-06-17 14:04:14 +0200

    Added hero and offer section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,100 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react';
+import styled from 'styled-components';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from '../templates/Layout';
+import Heading from '../components/atoms/Heading';
+import Text from '../components/atoms/Text';
+import HeroImg from '../assets/images/hero1.jpg';
+import Button from '../components/atoms/Button';
+import Offer from '../components/organisms/Offer/Offer';
+
+const StyledHeading = styled(Heading)`
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.white};
+  font-size: ${({ theme }) => theme.headingM};
+
+  @media (min-width: 1200px) {
+    font-size: ${({ theme }) => theme.headingL};
+  }
+  @media (min-width: 1400px) {
+    font-size: ${({ theme }) => theme.headingXL};
+  }
+
+  span {
+    color: ${({ theme }) => theme.yellow};
+  }
+`;
+
+const SectionWrapper = styled.section`
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
+
+const SectionContainer = styled.div`
+  position: relative;
+  background-image: url(${HeroImg});
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100%;
+  border-radius: 0 50px 0 50px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  padding: 100px 15px;
+
+  @media (min-width: 1200px) {
+    width: calc(100% - 20px);
+    height: calc(100% - 20px);
+    border-radius: 0 100px 0 100px;
+  }
+
+  & > * {
+    z-index: 10;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.7);
+    z-index: 1;
+  }
+`;
+
+const HeroText = styled(Text)`
+  font-size: ${({ theme }) => theme.bodyS};
+  margin: 30px 0;
+
+  @media (min-width: 1200px) {
+    font-size: ${({ theme }) => theme.headingS};
+  }
+`;
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+  <Layout title="Marketing internetowy">
+    <SectionWrapper>
+      <SectionContainer>
+        <StyledHeading>
+          Wszystkie cyfrowe usługi <br /> <span>w jednym miejscu</span>
+        </StyledHeading>
+        <HeroText className="text--white">
+          Poznaj naszych specjalistów z każdej branży!
+        </HeroText>
+        <Button link="/#offer" color="white">
+          SPRAWDŹ
+        </Button>
+      </SectionContainer>
+    </SectionWrapper>
+    <Offer />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
