15702e3dbf6a639dbcf77e30b31e13b59950dfb2
kryderius <krydegamer@gmail.com>
2021-07-01 15:28:59 +0200

    Added Create pages from templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,79 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`);
+
+const makeRequest = (graphql, request) =>
+  new Promise((resolve, reject) => {
+    // Query for nodes to use in creating pages.
+    resolve(
+      graphql(request).then((result) => {
+        if (result.errors) {
+          reject(result.errors);
+        }
+
+        return result;
+      })
+    );
+  });
+
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+  // Query for articles nodes to use in creating pages.
+
+  const getServices = makeRequest(
+    graphql,
+    `
+    {
+        allDatoCmsService {
+            edges {
+                node {
+                    slug
+                }
+            }
+        }
+    }
+    `
+  ).then((result) => {
+    // Create pages for each article.
+    result.data.allDatoCmsService.edges.forEach(({ node }) => {
+      createPage({
+        path: `/${node.slug}`,
+        component: path.resolve(`src/templates/Service.js`),
+        context: {
+          slug: node.slug,
+        },
+      });
+    });
+  });
+
+  const getSpecialists = makeRequest(
+    graphql,
+    `
+    {
+        allDatoCmsSpecialist {
+            edges {
+                node {
+                    name
+                }
+            }
+        }
+    }
+    `
+  ).then((result) => {
+    // Create pages for each article.
+    result.data.allDatoCmsSpecialist.edges.forEach(({ node }) => {
+      createPage({
+        path: `/specjalisci/${node.name.toLowerCase()}`,
+        component: path.resolve(`src/templates/Specialist.js`),
+        context: {
+          name: node.name,
+        },
+      });
+    });
+  });
+
+  return getServices, getSpecialists;
+};
