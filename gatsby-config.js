38631b0b25c830bb2a874e1285ddefd7c8b0a804
kryderius <krydegamer@gmail.com>
2021-07-12 12:21:05 +0200

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,18 +29,13 @@ module.exports = {
         },
       },
     },
-    //{
-    //  resolve: `gatsby-plugin-manifest`,
-    //  options: {
-    //    name: `gatsby-starter-default`,
-    //    short_name: `starter`,
-    //    start_url: `/`,
-    //    background_color: `#663399`,
-    //    theme_color: `#663399`,
-    //    display: `minimal-ui`,
-    //    icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //  },
-    //},
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        start_url: `/`,
+        icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
