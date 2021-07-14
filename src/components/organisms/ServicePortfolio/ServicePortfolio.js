d858bcdf4be64024106506ba9d4cf3194c13d1d8
kryderius <krydegamer@gmail.com>
2021-07-14 16:11:08 +0200

    Added portfolio lightbox

--- src/components/organisms/ServicePortfolio/ServicePortfolio.js
+++ src/components/organisms/ServicePortfolio/ServicePortfolio.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Heading from '../../atoms/Heading';
 import Text from '../../atoms/Text';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import Button from '../../atoms/Button';
+import CloseSVG from '../../../assets/svg/cross_black_sm.svg';
+import { Link } from 'gatsby';
 
 const PortfolioWrapper = styled.section``;
 
@@ -66,6 +68,7 @@ const ImageContainer = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
+  cursor: zoom-in;
 
   @media (min-width: 1200px) {
     width: 33.33%;
@@ -140,9 +143,147 @@ const HoverImg = styled.div`
   }
 `;
 
+const Lightbox = styled.div`
+  display: none;
+  //padding: 100px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  transform: translate(-50%, -50%);
+  z-index: 2000;
+
+  @media (min-width: 1200px) {
+    display: block;
+  }
+`;
+
+const LightboxWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 100px;
+  overflow-y: scroll;
+  position: relative;
+`;
+
+const CloseBtn = styled.div`
+  cursor: pointer;
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  border: none;
+  outline: none;
+  border-radius: 50px;
+  width: 50px;
+  height: 50px;
+  background-color: ${({ theme }) => theme.yellow};
+  z-index: 2000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 40%;
+  }
+`;
+
+const LightboxInfo = styled.div`
+  position: absolute;
+  top: 15px;
+  left: 100px;
+  z-index: 2010;
+  display: flex;
+`;
+
+const LightboxAuthorImg = styled.div`
+  width: 70px;
+  height: 70px;
+  border-radius: 50px;
+  overflow: hidden;
+  position: relative;
+
+  .img {
+    position: abolute;
+    top: 0;
+    left: 0;
+  }
+`;
+
+const LightboxInfoText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 30px;
+`;
+
+const LightboxInfoTitle = styled(Text)`
+  font-family: ${({ theme }) => theme.fontFamilyHeading};
+  font-size: ${({ theme }) => theme.headingS};
+  color: ${({ theme }) => theme.white};
+  margin: 0 0 10px 0;
+  text-align: left;
+`;
+
+const LightboxInfoAuthor = styled(Text)`
+  text-align: left;
+  color: ${({ theme }) => theme.white};
+  margin: 0;
+  font-weight: ${({ theme }) => theme.medium};
+
+  span {
+    font-weight: ${({ theme }) => theme.light};
+  }
+`;
+
+const StyledAuthorLink = styled(Link)`
+  color: ${({ theme }) => theme.yellow};
+`;
+
 const ServicePortfolio = ({ portfolio }) => {
+  const [openImage, setOpenImage] = useState(false);
+  const [activeImage, setActiveImage] = useState('');
+  const [activeAuthorImg, setActiveAuthorImg] = useState('');
+  const [activeAuthorName, setActiveAuthorName] = useState('');
+  const [activeTitle, setActiveTitle] = useState('');
+
+  const handleImageWrapper = (e, authorImg, title, name) => {
+    setOpenImage(!openImage);
+    setActiveImage(e);
+    setActiveAuthorImg(authorImg);
+    setActiveTitle(title);
+    setActiveAuthorName(name);
+  };
+
   return (
     <PortfolioWrapper>
+      {openImage && (
+        <Lightbox>
+          <LightboxInfo>
+            <LightboxAuthorImg>
+              {activeAuthorImg && <GatsbyImage image={activeAuthorImg} />}
+            </LightboxAuthorImg>
+            <LightboxInfoText>
+              <LightboxInfoTitle>{activeTitle}</LightboxInfoTitle>
+              <LightboxInfoAuthor>
+                <span>Autor:</span>{' '}
+                <StyledAuthorLink
+                  to={`/specjalisci/${activeAuthorName.toLowerCase()}`}
+                >
+                  {activeAuthorName}
+                </StyledAuthorLink>
+              </LightboxInfoAuthor>
+            </LightboxInfoText>
+          </LightboxInfo>
+          <CloseBtn onClick={() => setOpenImage(false)}>
+            <img src={CloseSVG} alt="" />
+          </CloseBtn>
+          <LightboxWrapper>
+            <GatsbyImage image={activeImage} alt="" />
+          </LightboxWrapper>
+        </Lightbox>
+      )}
       <PortfolioContainer>
         <HeadingWrapper className="service-heading--trigger">
           <StyledHeading as="h3">
@@ -165,7 +306,17 @@ const ServicePortfolio = ({ portfolio }) => {
         </HeadingWrapper>
         <ImagesWrapper data-aos="fade-up">
           {portfolio.edges.map((item, index) => (
-            <ImageContainer key={index}>
+            <ImageContainer
+              key={index}
+              onClick={(e) =>
+                handleImageWrapper(
+                  item.node.image.gatsbyImageData,
+                  item.node.authorImage.gatsbyImageData,
+                  item.node.title,
+                  item.node.author
+                )
+              }
+            >
               <GatsbyImage image={item.node.image.gatsbyImageData} />
               <ImageHoverInfo className="image-hover-info">
                 <HoverTitle>{item.node.title}</HoverTitle>
