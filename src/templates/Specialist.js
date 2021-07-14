6ea42157e2c16b13f7bcef7ef880fa21b83b5151
kryderius <krydegamer@gmail.com>
2021-07-14 16:25:58 +0200

    Added portfolio loghtbox

--- src/templates/Specialist.js
+++ src/templates/Specialist.js
@@ -321,6 +321,9 @@ export const query = graphql`
           image {
             gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
           }
+          authorImage {
+            gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
+          }
         }
       }
     }
