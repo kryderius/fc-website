aba10cd298098cde79a3b9953496df4f0c9927b5
kryderius <krydegamer@gmail.com>
2021-08-06 15:03:33 +0200

    Form submit configuration

--- src/components/organisms/ContactForm/ContactForm.js
+++ src/components/organisms/ContactForm/ContactForm.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Formik, Field, Form } from 'formik';
 import Text from '../../atoms/Text';
+import axios from 'axios';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 const FormWrapper = styled.div`
   input,
@@ -101,6 +103,7 @@ const LastLine = styled.div`
     }
   }
   button {
+    margin-top: 30px;
     @media (min-width: 768px) {
       margin-right: 10%;
     }
@@ -124,38 +127,191 @@ const LastLine = styled.div`
 
 const RequiredInfo = styled.div`
   position: absolute;
-  top: -20%;
+  top: -10%;
   right: 0;
+
+  @media (min-width: 1200px) {
+    top: -20%;
+  }
 `;
 
 const RequiredText = styled(Text)`
   font-size: ${({ theme }) => theme.bodyXS};
   margin: 0;
 `;
+const FormSubmitedInfo = styled.div`
+  visibility: hidden;
+  opacity: 0;
+  position: fixed;
+  bottom: 50px;
+  left: 50%;
+  width: 80%;
+  max-width: 500px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transform: translate(-50%, 20%);
+  background: ${({ theme }) => theme.white};
+  box-shadow: 0 4px 25px 0 rgba(0, 0, 0, 0.12);
+  border-radius: 20px;
+  transition: all 0.4s cubic-bezier(0.5, 1, 0.89, 1);
+  z-index: 1080;
+
+  &.formSubmitedInfo--open {
+    visibility: visible;
+    opacity: 1;
+    transform: translate(-50%, 0%);
+  }
+`;
+
+const FormSubmitedInfoReCaptcha = styled.div`
+  visibility: hidden;
+  opacity: 0;
+  position: fixed;
+  bottom: 50px;
+  left: 50%;
+  width: 80%;
+  max-width: 500px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transform: translate(-50%, 20%);
+  background: ${({ theme }) => theme.white};
+  box-shadow: 0 4px 25px 0 rgba(0, 0, 0, 0.12);
+  border-radius: 20px;
+  transition: all 0.4s cubic-bezier(0.5, 1, 0.89, 1);
+  z-index: 1080;
+
+  &.formSubmitedInfo--open {
+    visibility: visible;
+    opacity: 1;
+    transform: translate(-50%, 0%);
+  }
+`;
+
+const FormSubmitedInfoButton = styled.button`
+  width: 120px;
+  height: 40px;
+  background-color: ${({ theme }) => theme.white};
+  border: 2px solid ${({ theme }) => theme.yellow};
+  border-radius: 50px;
+  color: ${({ theme }) => theme.yellow};
+  cursor: pointer;
+  font-family: ${({ theme }) => theme.fontFamilyHeading};
+`;
+
+const LastLineReCaptcha = styled.div``;
 
 const ContactForm = () => {
+  const [openErrorMsg, setOpenErrorMsg] = useState(false);
+  const [isFormSubmited, setFormSubmited] = useState(false);
+  const [isRecaptchaTrue, setRecaptchaTrue] = useState(false);
+
+  const isReCAPTCHAVerifed = () => {
+    setRecaptchaTrue(true);
+  };
+
+  const isReCAPTCHAExpired = () => {
+    setRecaptchaTrue(false);
+  };
+
   return (
     <FormWrapper>
+      <FormSubmitedInfo
+        className={
+          isFormSubmited ? 'formSubmitedInfo--open' : 'formSubmitedInfo'
+        }
+      >
+        <Text>Pomyślnie wysłano wiadomość</Text>
+        <FormSubmitedInfoButton onClick={() => setFormSubmited(false)}>
+          Zamknij
+        </FormSubmitedInfoButton>
+      </FormSubmitedInfo>
+      <FormSubmitedInfoReCaptcha
+        className={openErrorMsg ? 'formSubmitedInfo--open' : 'formSubmitedInfo'}
+      >
+        <Text>Wypełnij pole ReCAPTCHA!</Text>
+        <FormSubmitedInfoButton onClick={() => setOpenErrorMsg(false)}>
+          Rozumiem
+        </FormSubmitedInfoButton>
+      </FormSubmitedInfoReCaptcha>
       <Formik
         initialValues={{
           firstName: '',
-          lastName: '',
           email: '',
           phone: '',
           needs: '',
+          message: '',
         }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
+        onSubmit={(values, actions) => {
+          if (!isRecaptchaTrue) {
+            setOpenErrorMsg(true);
+          } else {
+            axios.defaults.headers.post['Content-Type'] = 'application/json';
+            axios
+              .all([
+                axios.post(
+                  'https://formsubmit.co/ajax/5fbd87b9094f144a04faa702eb1e1518',
+                  {
+                    name: values.firstName,
+                    email: values.email,
+                    phone: values.phone ? values.phone : 'Nie podano',
+                    needs: values.needs,
+                    message: values.message,
+                  }
+                ),
+
+                axios.post(
+                  'https://formsubmit.co/ajax/m.gorska@freelanceconcept.pl',
+                  {
+                    name: values.firstName,
+                    email: values.email,
+                    phone: values.phone ? values.phone : 'Nie podano',
+                    needs: values.needs,
+                    message: values.message,
+                  }
+                ),
+              ])
+              .then((response) => console.log('Wysłano email'))
+              .catch((error) => {
+                alert(
+                  'Wystąpił błąd. Wyślij wiadomość ręcznie za pomocą podanego emaila'
+                );
+                setFormSubmited(false);
+              });
+            actions.setSubmitting(false);
+            setFormSubmited(true);
+            setOpenErrorMsg(false);
+          }
         }}
       >
         <Form>
           <Line>
-            <Field id="firstName" name="firstName" placeholder="Imię*" />
-            <Field id="lastName" name="lastName" placeholder="Nazwisko*" />
+            <Field
+              id="firstName"
+              name="firstName"
+              placeholder="Imię*"
+              required
+            />
+            <Field
+              id="lastName"
+              name="lastName"
+              placeholder="Nazwisko*"
+              required
+            />
           </Line>
           <Line>
-            <Field id="email" name="email" placeholder="E-mail*" type="email" />
+            <Field
+              id="email"
+              name="email"
+              placeholder="E-mail*"
+              type="email"
+              required
+            />
             <Field id="phone" name="phone" placeholder="Telefon" />
           </Line>
           <Field
@@ -163,16 +319,19 @@ const ContactForm = () => {
             id="needs"
             name="needs"
             placeholder="Czego potrzebujesz?*"
+            required
           >
             <option>Czego potrzebujesz?*</option>
-            <option>1</option>
-            <option>1</option>
+            <option>Chcę zbudować markę</option>
+            <option>Chcę więcej sprzedawać</option>
+            <option>Zadbajcie o moj wizerunek</option>
           </Field>
           <Field
             as="textarea"
             id="message"
             name="message"
             placeholder="Twoja wiadomość*"
+            required
           ></Field>
 
           <LastLine>
@@ -182,27 +341,36 @@ const ContactForm = () => {
                 as="input"
                 id="agreement"
                 name="agreement"
+                required
               />
               Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z
               ustawą o ochronie danych osobowych w związku z wysłaniem zapytania
               przez formularz kontaktowy. Podanie danych jest dobrowolne, ale
               niezbędne do przetworzenia zapytania
             </label>
-            <button type="submit">
-              Wyślij{' '}
-              <svg
-                width="33"
-                height="21"
-                viewBox="0 0 33 21"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M19.5919 2.90812L25.1006 8.4375H0V12.5625H25.1006L19.6838 18L22.5919 20.9081L33 10.5L22.5 0L19.5919 2.90812Z"
-                  fill="#FEDD5A"
-                />
-              </svg>
-            </button>
+            <LastLineReCaptcha>
+              <ReCAPTCHA
+                sitekey="6Lek2OIbAAAAAMrhaauHps7dhu_6CCOSz6HkD0hz"
+                onChange={isReCAPTCHAVerifed}
+                onExpired={isReCAPTCHAExpired}
+                render="onload"
+              />
+              <button type="submit">
+                Wyślij{' '}
+                <svg
+                  width="33"
+                  height="21"
+                  viewBox="0 0 33 21"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M19.5919 2.90812L25.1006 8.4375H0V12.5625H25.1006L19.6838 18L22.5919 20.9081L33 10.5L22.5 0L19.5919 2.90812Z"
+                    fill="#FEDD5A"
+                  />
+                </svg>
+              </button>
+            </LastLineReCaptcha>
             <RequiredInfo>
               <RequiredText>Pola oznaczone * są obowiązkowe</RequiredText>
             </RequiredInfo>
